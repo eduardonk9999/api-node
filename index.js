d7a09a1658e196396701d90275d4ebb2901017fb
eduardonk9999 <thiago.novak7@gmail.com>
2021-08-13 17:37:06 -0300

    Add, get

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const morgan = require('morgan')
+const bodyparser = require('body-parser')
+const cors = require('cors')
+
+const app = express()
+
+app.use(morgan('dev'))
+app.use(bodyparser.urlencoded({ extended: false }))
+app.use(express.json())
+app.use(cors())
+
+
+let db = [
+  { '1': {name: 'Cliente 1', Idade: '20'} },
+  { '2': {name: 'Cliente 2', Idade: '20'} },
+  { '3': {name: 'Cliente 3', Idade: '20'} }
+]
+
+app.get('/', (req, res) => {
+  return res.json(db)
+})
+
+app.listen(21262, () => {
+  console.log('Foi Familia!')
+})
